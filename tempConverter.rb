f2ce7a8c01f5b5622d2071001769e833072ee1b9
stevnzhang <roboticdraqo@gmail.com>
2022-01-27 14:26:15 -0500

    fixed numbers less than abs zero

--- tempConverter.rb
+++ tempConverter.rb
@@ -1,5 +1,6 @@
 def convert(temp)
     return "Temperature must be an integer" unless temp.class == Integer
+    return "Temperature below Absolute Zero" if temp < -474
     5 * (temp - 32) / 9
 end
 
@@ -9,3 +10,4 @@ puts convert(32)
 puts convert(50)          
 puts convert(212)
 puts convert("zero")
+puts convert(-500)
