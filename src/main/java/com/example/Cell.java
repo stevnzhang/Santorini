dd8da74825ed96494536115eb0cbdc2d3abe2a91
stevnzhang <roboticdraqo@gmail.com>
2022-02-22 03:22:38 -0500

    updated cell implementation

--- src/main/java/com/example/Cell.java
+++ src/main/java/com/example/Cell.java
@@ -1,24 +1,48 @@
 package com.example;
 
-public final class Cell {
+public class Cell {
     private int row;
     private int col;
-    private Cell cell;
-    private Tower tower;
+    private int numLevels;
+    private boolean hasDome;
     private boolean occupied;
 
-    public Cell(boolean occupied) {
-        this.cell = num;
-        this.tower =
+    private static final int DOME_LEVEL = 4;
+
+    public Cell(int num, boolean occupied) {
+        this.numLevels = num;
         this.occupied = occupied;
     }
 
-    public int getHeight { return (int) this.cell; }
+    public int getRow() { return this.row; }
+
+    public int getCol() { return this.col; }
+
+    public int getLevels() { return this.numLevels; }
+
+    public void setUnoccupied() { this.occupied = false; }
+
+    public void setOccupied() { this.occupied = true; }
 
     /**
-     * Checks if the cell has another worker in it
-     *
-     * @return occupancy
+     * Adds a block on to the cell
      */
-    public boolean occupancy { return this.occupied; }
+    public void addLevel() {
+        this.numLevels += 1;
+        if (this.numLevels == DOME_LEVEL) { this.hasDome = true; }
+    }
+
+    /**
+     * Checks if the cell already has a worker in it
+     *
+     * @return {@code true} if the cell is occupied
+     */
+    public boolean occupancy() { return this.occupied; }
+
+    /**
+     * Checks if the cell has a dome on it
+     *
+     * @return {@code true} if the cell has a dome
+     */
+    public boolean hasDome() { return this.hasDome; }
 }
