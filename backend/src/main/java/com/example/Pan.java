3b11a7d41fc47f5c2f80c5d2f05889ab3e0a16b2
stevnzhang <roboticdraqo@gmail.com>
2022-03-21 16:17:13 -0400

    implemented strat. pattern for Pan

--- backend/src/main/java/com/example/Pan.java
+++ backend/src/main/java/com/example/Pan.java
@@ -10,14 +10,20 @@ public class Pan implements GodCard {
      * @param player the current player.
      */
     @Override
-    public void gameOver(Player player) {
-        if (player.getWorker1().getHeight() == WIN_HEIGHT ||
-            player.getWorker2().getHeight() == WIN_HEIGHT ||
-            getWorker_dy() >= 2) { // The currPlayer has a worker at level 3, or they recently moved their worker 2 or more levels down
-            winner = player;
-            gameOver = true;
-        }
+    public boolean gameOver(Player player) {
+        Worker worker1 = player.getWorker1();
+        Worker worker2 = player.getWorker2();
+        int worker_dy1 = worker1.getPrevHeight() - worker1.getHeight();
+        int worker_dy2 = worker2.getPrevHeight() - worker1.getHeight();
 
+        // The currPlayer has a worker at level 3, or they recently moved their worker 2 or more levels down
+        if ((worker1.getHeight() == WIN_HEIGHT && !worker1.isForced()) ||
+            (worker2.getHeight() == WIN_HEIGHT && !worker2.isForced()) ||
+            (worker_dy1 >= 2 && !worker1.isForced()) ||
+            (worker_dy2 >= 2 && !worker2.isForced()) ) {
+             return true;
+        }
+        return false;
     }
 
 }
