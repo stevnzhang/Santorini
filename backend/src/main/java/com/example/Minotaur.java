ba87b17ccaf42b81b37912c175f8165c08e3a5b8
stevnzhang <roboticdraqo@gmail.com>
2022-03-28 16:01:45 -0400

    connected UI with minotaur backend

--- backend/src/main/java/com/example/Minotaur.java
+++ backend/src/main/java/com/example/Minotaur.java
@@ -3,10 +3,10 @@ package com.example;
 public class Minotaur implements GodCard {
 
     // Helper function to find the cell the opponent worker will be pushed to.
-    private Cell minotaurHelper(Cell src, Cell tgt, Cell[][] board) {
+    private Cell minotaurHelper(Cell src, Cell tgt) {
         int srcRow = src.getRow();
-        int tgtRow = tgt.getRow();
         int srcCol = src.getCol();
+        int tgtRow = tgt.getRow();
         int tgtCol = tgt.getCol();
         int behindRow = tgtRow;
         int behindCol = tgtCol;
@@ -19,21 +19,22 @@ public class Minotaur implements GodCard {
         if (srcCol < tgtCol)
             behindCol += 1;
         else if (srcCol > tgtCol)
-            behindCol -= tgtCol - 1;
+            behindCol -= 1;
 
-        return board[behindRow][behindCol];
+        return new Cell(behindRow, behindCol);
     }
 
     // Helper function that moves the workers and updates the board state (To make code more readable and reusable)
     private void pushWorkerHelper(Cell src, Cell tgt, Worker worker, Cell[][] board) {
         int row = tgt.getRow();
         int col = tgt.getCol();
-        Cell behind = minotaurHelper(src, tgt, board);
+        Cell behind = minotaurHelper(src, tgt);
         Worker opponentWorker = tgt.getWorker();
 
         board[opponentWorker.getRow()][opponentWorker.getCol()].setWorker(null);
         opponentWorker.moveWorker(behind.getRow(), behind.getCol(), board);
         board[behind.getRow()][behind.getCol()].setWorker(opponentWorker);
+        opponentWorker.setForced(true);
 
         board[worker.getRow()][worker.getCol()].setWorker(null);
         worker.moveWorker(tgt.getRow(), tgt.getCol(), board);
@@ -59,10 +60,11 @@ public class Minotaur implements GodCard {
         int originalCol = worker.getCol();
         Cell src = board[originalRow][originalCol];
         Cell tgt = board[row][col];
-        Cell behind = minotaurHelper(src, tgt, board);
+        Cell behind = minotaurHelper(src, tgt);
         Worker opponentWorker = tgt.getWorker();
 
         if (!tgt.occupancy()) { // Default move - No pushing
+            playerCheck(row, col, board); // Throw error is trying to move to an occupied cell
             if (checkLegalMove(row, col, worker, board)) {
                 if (player.getWorker1() == worker || player.getWorker2() == worker) {
 //                    worker.setPrevHeight(worker.getHeight()); Don't need this because we don't need to track dy for pan's power
@@ -73,10 +75,10 @@ public class Minotaur implements GodCard {
 //            worker.setForced(false); Don't need this because we don't have Pan card
         } else {
             // Target cell cannot be out of bounds or have a dome on it, but it can have a worker on it
-            basicLegalChecks(row, col, board);
+            checkLegalMove(row, col, worker, board);
 
             // Player cannot push its own worker
-            if (row == opponentWorker.getRow() && col == opponentWorker.getCol()) { throw new InvalidMoveException("Cannot push your own worker!"); }
+            if (originalRow == opponentWorker.getRow() && originalCol == opponentWorker.getCol()) { throw new InvalidMoveException("Cannot push your own worker!"); }
 
             // Behind cell cannot be out of bounds, have a dome, or have another player on it
             basicLegalChecks(behind.getRow(), behind.getCol(), board); // Throws error and end game if illegal move, otherwise continue (doesn't check dome)
