5c0bdff68b88a09a72a781587ebc2a9f077a52a6
stevnzhang <roboticdraqo@gmail.com>
2022-03-28 08:18:28 -0400

    updated app to handle play requests

--- backend/src/main/java/com/example/App.java
+++ backend/src/main/java/com/example/App.java
@@ -1,49 +1,66 @@
-//package game;
-//
-//import java.io.IOException;
-//import java.util.Map;
-//import fi.iki.elonen.NanoHTTPD;
-//
-//public class App extends NanoHTTPD {
-//
-//    public static void main(String[] args) {
-//        try {
-//            new App();
-//        } catch (IOException ioe) {
-//            System.err.println("Couldn't start server:\n" + ioe);
-//        }
-//    }
-//
-//    private Game game;
-//
-//    public App() throws IOException {
-//        super(8080);
-//
-//        this.game = new Game();
-//
-//        start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
-//        System.out.println("\nRunning!\n");
-//    }
-//
-//    @Override
-//    public Response serve(IHTTPSession session) {
-//        String uri = session.getUri();
-//        Map<String, String> params = session.getParms();
-//        if (uri.equals("/newgame")) {
-//            this.game = new Game();
-//        } else if (uri.equals("/play")) {
-//            this.game = this.game.play(Integer.parseInt(params.get("x")), Integer.parseInt(params.get("y")));
-//        } else if (uri.equals("/undo")) {
-//            this.game = this.game.undo();
-//        }
-//        // Extract the view-specific data from the game and apply it to the template.
-//        GameState gameplay = GameState.forGame(this.game);
-//        return newFixedLengthResponse(gameplay.toString());
-//    }
-//
-//    public static class Test{
-//        public String getText() {
-//            return "Hello World!";
-//        }
-//    }
-//}
+package com.example;
+
+import java.io.IOException;
+import java.util.Map;
+
+import fi.iki.elonen.NanoHTTPD;
+
+public class App extends NanoHTTPD {
+
+    public static void main(String[] args) {
+        try {
+            new App();
+        } catch (IOException ioe) {
+            System.err.println("Couldn't start server:\n" + ioe);
+        }
+    }
+
+    private Game game;
+
+    public App() throws IOException {
+        super(8080);
+
+        Player player1 = new Player("1");
+        Player player2 = new Player("2");
+        this.game = new Game(player1, player2);
+        this.game.setGodCard(new NoCard(), game.getPlayer1());
+        this.game.setGodCard(new NoCard(), game.getPlayer2());
+
+        start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+        System.out.println("\nRunning!\n");
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        String uri = session.getUri();
+        Map<String, String> params = session.getParms();
+        Player currentPlayer = this.game.getCurrentPlayer();
+        if (uri.equals("/newgame")) {
+            Player player1 = new Player("1");
+            Player player2 = new Player("2");
+            this.game = new Game(player1, player2);
+            this.game.setGodCard(new NoCard(), this.game.getPlayer1());
+            this.game.setGodCard(new NoCard(), this.game.getPlayer2());
+        } else if (uri.equals("/play")) {
+            Cell position = new Cell(Integer.parseInt(params.get("x")), Integer.parseInt(params.get("y")));
+            try {
+                this.game.initiatePlayer(position, this.game.getCurrentPlayer());
+                if (currentPlayer.getWorker1() != null && currentPlayer.getWorker2() != null && !game.allWorkersPlaced()) {
+                    this.game.setCurrentPlayer();
+                }
+                if (game.allWorkersPlaced()) this.game.setCurrentPlayer();
+            } catch (InvalidMoveException e) {
+                e.printStackTrace();
+            }
+        }
+        // Extract the view-specific data from the game and apply it to the template.
+        GameState gameplay = GameState.forGame(this.game);
+        return newFixedLengthResponse(gameplay.toString());
+    }
+
+    public static class Test{
+        public String getText() {
+            return "Hello World!";
+        }
+    }
+}
